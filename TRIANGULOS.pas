375179e55993d24f8d1ff776247a1a1f75c27c71
DICROMAN <dayermanvride@gmail.com>
2016-06-13 15:09:41 -0300

    Calendario Pascal UTFSM

--- TRIANGULOS.pas
+++ TRIANGULOS.pas
@@ -7,54 +7,33 @@ USES
 
 
 CONST
-	(*
-	* Nombre de los meses para comparar con el requerimiento del usuario y poder generar 
-	* calendario para el mes indicado.
-	*)
-	monthNames : Array[1..12] of String = ('ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 
+
+	Nmes : Array[1..12] of String = ('ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO',
 		'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE');
 
-	(*
-	* Este arreglo contiene datos sobre cantidad de dias de cada mes (primera columna) y 
-	* posicion en que inicia cada mes en el calendario (segunda columna).
-	* Las filas son correspondiente a cada mes del a๑o.
-	*)
-	meses : Array[1..12,1..2] of Byte = ((31,4), (28,7), (31,7), (30,3), (31,5), (30,1), (31,3), 
+	meses : Array[1..12,1..2] of Byte = ((31,4), (28,7), (31,7), (30,3), (31,5), (30,1), (31,3),
 		(31,6), (30,2), (31,4), (30,7), (31,2));
 
-	(*
-	* Este arreglo contiene los dias feriados de cada mes almacenado en las columnas.
-	* Las filas son correspondiente a cada mes del a๑o.
-	*)
-	feriados : Array[1..12,1..3] of Byte = 	((1,0,0), (0,0,0), (0,0,0), (1,10,0), (1,21,0), 
+
+	feriados : Array[1..12,1..3] of Byte = 	((1,0,0), (0,0,0), (0,0,0), (1,10,0), (1,21,0),
 		(29,0,0), (16,0,0), (15,0,0), (18,19,0), (12,31,0), (1,0,0), (8,13,25));
 
-    {Color para dํas feriados y domingo (RED: const 4, LightRed: const 12).}
+
 	holidayColor : Byte = RED;
-
-	{Color para fondo de pantalla.}
-	bgColor : Byte = WHITE;
-
-	{Color para texto normal.}
+	bgColor : Byte = 11;
 	normalText : Byte = BLACK;
 
 
 VAR
-	{Almacena la estructura final del calendario.}
-	calendario: Array[1..6,1..7] of ShortInt;	
 
-	{Seleccion de mes a amostrar.}
-	month : String[11];	
+	calendario: Array[1..6,1..7] of ShortInt;
 
-	{err: Identifica cuando se a ingresado un nombre de mes no vแlido. i: Itera en el rango [1,12] 
-	para buscar coincidencia de meses.}
-	i, err : Byte;	
+	mes : String[11];
+
+	i, err : Byte;
 
 
-(*
-* Procedimiento para colocar el arreglo calendario en su estado por defecto.
-*)
-procedure defaultCalendar;	
+procedure Calendar0;
 	VAR
 		i, j : Byte;
 
@@ -65,32 +44,28 @@ procedure defaultCalendar;
 	END;
 
 
-(*
-* Formato de tabla para calendario de 5 semanas.
-*)
-procedure drawFiveRows;
+
+procedure cincofilas;
 	VAR
-		(*
-		* i, j variables para iteraci๓n.
-		* x, y variables para posicionar los n๚meros de los dํas en su correspondiente casilla.
-		* dia se utiliza para controlar la cantidad de dํas que tiene el mes solicitado.
-		*)
+
 		i, j, x, y, dia : ShortInt;
 
-	BEGIN	
-writeln('ษอออออออออหอออออออออหอออออออออหอออออออออหออออออออออหอออออออออหอออออออออป ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสออออออออออสอออออออออสอออออออออผ ');
+	BEGIN
+         textbackground(7);
+        textcolor(15);
+writeln('ษอออออออออหอออออออออหอออออออออหอออออออออหอออออออออหอออออออออหอออออออออป ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสอออออออออสอออออออออสอออออออออผ ');
 
 		GotoXY(4,5);	Write('LUNES');
 		GotoXY(13,5);	Write('MARTES');
@@ -100,73 +75,69 @@ writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสออออออออออสอออออออออสอออออออออ
 		GotoXY(53,5);	Write('SABADO');
 		GotoXY(63,5);	Write('DOMINGO');
 
-		y := 5; {Movimiento en filas.}
-		x := -5; {Movimiento en columnas.}
-		dia := 0;	{Contador de dias.}
+		y := 5;
+		x := -5;
+		dia := 0;
 
 		for i := 1 to 5 do
-			BEGIN
-				y := y + 2;		
-				for j := 1 to 7 do
+		   BEGIN
+			y := y + 2;
+			for j := 1 to 7 do
+			  BEGIN
+			     x := x + 10;
+				GotoXY(x,y);
+				if (calendario[i,j]<>0) then
 					BEGIN
-						x := x + 10;			
-						GotoXY(x,y); 			
-						if (calendario[i,j]<>0) then
-							BEGIN				 
-								if (calendario[i,j]<0) OR (x = 65)then
-									BEGIN
-										if calendario[i,j]<0 then
-											BEGIN
-												TextColor(holidayColor);
-												Write(calendario[i,j]*-1)
-											END
-										else
-											BEGIN
-												TextColor(holidayColor);
-												Write(calendario[i,j])
-											END;
-									END
-								else
-									BEGIN
-										TextColor(normalText);
-										Write(calendario[i,j]);
-									END;
-							END;			 
-						dia := dia + 1;
+					  if (calendario[i,j]<0) OR (x = 65)then
+						BEGIN
+						   if calendario[i,j]<0 then
+							BEGIN
+							  TextColor(holidayColor);
+							  Write(calendario[i,j]*-1)
+							END
+							else
+							BEGIN
+							  TextColor(holidayColor);
+							  Write(calendario[i,j])
+							END;
+							END
+							else
+							BEGIN
+							  TextColor(normalText);
+							  Write(calendario[i,j]);
+						END;
 					END;
-				x := -5;
-			END;
+			     dia := dia + 1;
+			  END;
+			x := -5;
+		    END;
 		TextColor(normalText);
 	END;
 
 
-(*
-* Formato de tabla para calendario de 6 semanas.
-*)
-procedure drawSixRows;
+
+procedure seisfilas;
 	VAR
-		(*
-		* i, j variables para iteraci๓n.
-		* x, y variables para posicionar los n๚meros de los dํas en su correspondiente casilla.
-		* dia se utiliza para controlar la cantidad de dํas que tiene el mes solicitado.
-		*)
+
 		x, y, j, i, dia : ShortInt;
 	BEGIN
-writeln('ษอออออออออหอออออออออหอออออออออหอออออออออหออออออออออหอออออออออหอออออออออป ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮออออออออออฮอออออออออฮอออออออออน ');
-writeln('บ         บ         บ         บ         บ          บ         บ         บ ');
-writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสออออออออออสอออออออออสอออออออออผ ');
+        textbackground(7);
+        textcolor(15);
+writeln('ษอออออออออหอออออออออหอออออออออหอออออออออหอออออออออหอออออออออหอออออออออป ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ฬอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออฮอออออออออน ');
+writeln('บ         บ         บ         บ         บ         บ         บ         บ ');
+writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสอออออออออสอออออออออสอออออออออผ ');
 		GotoXY(4,5);	Write('LUNES');
 		GotoXY(13,5);	Write('MARTES');
 		GotoXY(22,5);	Write('MIERCOLES');
@@ -175,182 +146,160 @@ writeln('ศอออออออออสอออออออออสอออออออออสอออออออออสออออออออออสอออออออออสอออออออออ
 		GotoXY(53,5);	Write('SABADO');
 		GotoXY(63,5);	Write('DOMINGO');
 
-		y := 5;		{Movimiento en filas.}
-		x := -5;	{Movimiento en columnas.}
-		dia := 0;	{Contador de dias.}
+		y := 5;
+		x := -5;
+		dia := 0;
 
 		for i := 1 to 6 do
+		  BEGIN
+		    y := y + 2;
+		    for j := 1 to 7 do
 			BEGIN
-				y := y + 2;		
-				for j := 1 to 7 do
-					BEGIN
-						x := x + 10;			
-						if (calendario[i,j]<>0) then
+			  x := x + 10;
+			  if (calendario[i,j]<>0) then
+				BEGIN
+				  GotoXY(x,y);
+				  if (calendario[i,j]<0) OR (x = 65) then
+						BEGIN
+						  if calendario[i,j]<0 then
 							BEGIN
-								GotoXY(x,y); 
-								if (calendario[i,j]<0) OR (x = 65) then
-									BEGIN
-										if calendario[i,j]<0 then
-											BEGIN
-												TextColor(holidayColor);
-												Write(calendario[i,j]*-1)
-											END
-										else
-											BEGIN
-												TextColor(holidayColor);
-												Write(calendario[i,j]);
-											END;
-									END
-								else
-									BEGIN
-									TextColor(normalText);
-									Write(calendario[i,j]);
-									END;
-							END; 
-						dia := dia + 1;
-					END;
-				x := -5;
+							  TextColor(holidayColor);
+							  Write(calendario[i,j]*-1)
+							END
+							else
+							  BEGIN
+							    TextColor(holidayColor);
+							    Write(calendario[i,j]);
+							  END;
+							END
+					else
+							BEGIN
+							  TextColor(normalText);
+							  Write(calendario[i,j]);
+						END;
+				END;
+			  dia := dia + 1;
 			END;
+		    x := -5;
+		  END;
 		TextColor(normalText);
 	END;
 
 
 
-(*
-* Recibe como parametro el numero del mes.
-*)
-procedure generarCalendario(mes : Byte);
+
+procedure Calendarioej(mes : Byte);
 	VAR
 		i, j, dia, feriado : Byte;
 
 	BEGIN
-		dia := 1;
-		feriado := 1;
-			for i := 1 to 6 do
+          dia := 1;
+	  feriado := 1;
+	   for i := 1 to 6 do
+		BEGIN
+		  for j := 1 to 7 do
+			BEGIN
+			  if (j >= meses[mes, 2]) AND (i = 1)  OR (i>1) AND (dia <= meses[mes, 1]) then
 				BEGIN
-					for j := 1 to 7 do	
-						BEGIN 		
-							if (j >= meses[mes, 2]) AND (i = 1)  OR (i>1) AND (dia <= meses[mes, 1]) then				
-								BEGIN
-									if dia = feriados[mes, feriado] then
-										BEGIN
-											calendario[i, j] := dia*-1;
-											feriado := feriado + 1;
-										END
-									else
-										calendario[i, j] := dia;
+				  if dia = feriados[mes, feriado] then
+					BEGIN
+					  calendario[i, j] := dia*-1;
+					  feriado := feriado + 1;
+					END
+				else
+					calendario[i, j] := dia;
 
-									dia := dia + 1;						
-								END;					
-						END;		
+				dia := dia + 1;
 				END;
+			END;
+		END;
 	END;
 
 
-(*
- * Funcion desarrollada por drdebcol: http://pro9ramming.com/uppercase-and-lowercase-string-t-503.html
- * La funcion Dec(a,b), decrementa el primer parametro en la cantidad especificada en el segundo parametro.
- * Si el segundo parametro no es especificado, toma por defecto el valor de 1.
- * Mแs informaci๓n sobre el funcionamiento de Dec() en http://www.freepascal.org/docs-html/rtl/system/dec.html
- *
- * En este caso, se utiliza un decremento de 32 para poder hacer la conversion de minuscula a mayuscula
- * guiandose por la tabla ASCII.
- * Ej: "A" tiene un valor decima de 65, mientras que "a" tiene un valor decimal de 97. Por lo que al restar
- * el valor decimal 32, es posible reemplazar su valor por el valor de su letra correspondiente en mayuscula.
-*)
-function uppercase(cadena : String) : String;
+
+function MAYUS(texto : String) : String;
 	VAR
-		i : Byte;
+
+		AUX : String;
+		pos : Byte ;
 	BEGIN
-		for i := 1 to length(cadena) do
-		    if (cadena[i] >= 'a') and (cadena[i] <= 'z') then
-		    	dec(cadena[i], 32);
-		    uppercase := cadena;
+		AUX :='' ;
+		for pos := 1 to length(texto) do
+		    AUX := AUX + Upcase(texto[pos]);
+
+		MAYUS := AUX
 	END;
 
-(*
-* Pantalla de inicio del calendario.
-*)
-procedure start;
-	BEGIN
-		WriteLn;
-		WriteLn;
-		WriteLn('        oMs                sM+    ');
-		WriteLn('    dooosMsoooooooooooooooosM+oooh');
-		WriteLn('    M`  +No                oN+   M');
-		WriteLn('    M`                           M');
-		WriteLn('    MssssssssssssssssssssssssssssM');
-		WriteLn('    M````````````````````````````M');
-		WriteLn('    M`                          `M');
-		WriteLn('    M`                          `M');
-		WriteLn('    M`        BIENVENIDO        `M');
-		WriteLn('    M`             A            `M');
-		WriteLn('    M`        CALENDARIO        `M');
-		WriteLn('    M`                          `M');
-		WriteLn('    M`                          `M');
-		WriteLn('    M`                          `M');
-		WriteLn('    MssssssssssssssssssssssssssssM');	
-		WriteLn;
-		Delay(3500);
-	END;
 
 
 BEGIN
 	TextBackground(bgColor);
 	TextColor(normalText);
-	clrscr;	
-	start;
+	clrscr;
+
 	REPEAT
-		defaultCalendar;
+		Calendar0;
 		REPEAT
-			(*
-			*	Este ciclo se repite hasta que el usuario ingrese un nombre correcto de mes.
-			*	De ingresar un nombre no valido, se devuelve  un mensaje de error.
-			*)
+
 	        clrscr;
 			WriteLn;
+			WriteLn('Bienvenido a Calendario') ;
 			Write(' Escriba mes a consultar: ');
-			ReadLn(month);
-			month := uppercase(month);
+			ReadLn(mes);
+			mes := MAYUS(mes);
 	        i := 0;
 	        err := 0;
 	        REPEAT
 	            i := i + 1;
-	            if month = monthNames[i] then
+	            if mes = Nmes[i] then
 		            BEGIN
 			            clrscr;
 			            WriteLn;
-		                WriteLn(' Mes consultado: ', month, '/2015');
+		                WriteLn(' Mes consultado: ', mes, '');
 					    WriteLn;
 		                err := 1;
-		                generarCalendario(i);
+		                Calendarioej(i);
 			        END;
-	        UNTIL (month = monthNames[i]) OR (i > 12);
+	        UNTIL (mes = Nmes[i]) OR (i > 12);
 
 	        if err = 0 then
 		        BEGIN
 		            WriteLn;
 		            WriteLn(' Error. Intente nuevamente.');
-		            Delay(2500);
+		            Delay(3000);
 		        END;
 
-        UNTIL(month = 'ENERO') OR (month = 'FEBRERO') OR (month = 'MARZO') OR (month = 'ABRIL') OR
-        (month = 'MAYO') OR (month = 'JUNIO') OR (month = 'JULIO') OR (month = 'AGOSTO') OR
-        (month = 'SEPTIEMBRE') OR (month = 'OCTUBRE') OR (month = 'NOVIEMBRE') OR (month = 'DICIEMBRE');
+        UNTIL(mes = 'ENERO') OR (mes = 'FEBRERO') OR (mes = 'MARZO') OR (mes = 'ABRIL') OR
+        (mes = 'MAYO') OR (mes = 'JUNIO') OR (mes = 'JULIO') OR (mes = 'AGOSTO') OR
+        (mes = 'SEPTIEMBRE') OR (mes = 'OCTUBRE') OR (mes = 'NOVIEMBRE') OR (mes = 'DICIEMBRE');
 
 		if calendario[6,1] = 0 then
-			drawFiveRows
+			cincofilas
 		else
-			drawSixRows;
+			seisfilas;
 
-		GotoXY(1,20); 
-		WriteLn('_____________________________________________________________________________');
-        Write(' Para consultar otro mes presione ENTER. Para salir escriba "N" y confirme: ');
-		ReadLn(month);
-		month := UpperCase(month);
-	UNTIL(month = 'N');
+		GotoXY(1,20);
+         textbackground(bgColor);
+        Write(' Para consultar otro mes presione ENTER. Para salir escriba "S" : ');
+		ReadLn(mes);
+		mes := MAYUS(mes);
+	UNTIL(mes = 'S');
 
 	WriteLn;
+
+	Delay(4500);
+	WriteLn('  Este Clendario se ha traido a ustedes con la colaboracion de: ');
+	WriteLn('                          Diego Mendoza  ');
+	Delay(5000);
+	WriteLn('                          Sebastian Pulgar  ');
+	Delay(5000);
+	WriteLn('                          Rosa Pulgar  ');
+	Delay(5000);
+	WriteLn('                                Gracias...');
+	Delay(7000);
+        textcolor(LightBlue);
 	WriteLn(' Saliendo del programa...');
-	Delay(2000);
+	Delay(4500);
 END.
+
+
